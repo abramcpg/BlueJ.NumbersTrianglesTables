f3e007aa659e7d9dfcb2e015b33073ab107fd397
Abram <50758769+abramcpg@users.noreply.github.com>
2019-05-28 18:18:59 -0400

    Update NumberUtilities.java

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,32 +1,79 @@
 
 
 
-public class NumberUtilities {
+public class NumberUtilities 
+{
 
-    public static String getRange(int stop) {
-        return null;
+    public static String getRange(int stop) 
+    {
+        String answer = "";
+        for (int i = 0; i < stop; i++)
+        {
+            answer = answer + i;
+        }
+
+        return answer;
     }
 
-    public static String getRange(int start, int stop) {
-        return null;
+    public static String getRange(int start, int stop) 
+    {
+        String answer = "";
+        for (int i = start; i < stop; i++)
+        {
+            answer = answer + i;
+        }
+        return answer;
     }
 
 
-    public static String getRange(int start, int stop, int step) {
-        return null;
+    public static String getRange(int start, int stop, int step) 
+    {
+        String answer = "";
+        for (int i = start; i < stop; i += step)
+        {
+            answer = answer + i;
+        }
+        return answer;
     }
 
-    public static String getEvenNumbers(int start, int stop) {
-        return null;
+    public static String getEvenNumbers(int start, int stop) 
+    {
+        String answer = "";
+        for (int i = start; i < stop; i++)
+        {
+            if ((i % 2 == 0))
+            {
+                answer = answer + i;
+            }
+        }
+        return answer;
+    }
+    public static String getOddNumbers(int start, int stop) 
+    {
+        String answer = "";
+        for (int i = start; i < stop; i++)
+        {
+            
+            if (!(i % 2 == 0))
+            {
+                answer = answer + i;
+            }
+            
+        } 
+        
+        return answer;
     }
 
 
-    public static String getOddNumbers(int start, int stop) {
-        return null;
-    }
-
-
-    public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+    public static String getExponentiations(int start, int stop, int exponent) 
+    {
+        String answer = ""; //fail square and cube tests
+        double tempAnswer;
+        for (int i = start; i <= stop; i++)
+        {
+            tempAnswer = Math.pow(i,exponent);
+            answer = answer + tempAnswer;
+        }
+        return answer;
     }
 }

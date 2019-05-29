2d446a489164c90aa69606d266ca28c9e9468e89
Abram <50758769+abramcpg@users.noreply.github.com>
2019-05-29 17:12:58 -0400

    Update TableUtilities.java

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,62 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        Integer height = 4; //tests are wrong
+        Integer width = 4;
+        String answer = "";
+        Integer row = 0;
+        Integer column = 1;
+        Integer print = 1;
+        for (int i = 0; i < height; i++){ //vertical
+            for (int j = 0; j < width; j++){ //horizontal
+                row++;
+                print = row * column;
+                answer += " " + print + " |";
+            }
+            column++;
+            answer += "\n";
+            row = 0;
+        }
+        return answer;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        Integer height = 9; //tests are wrong
+        Integer width = 9;
+        String answer = "";
+        Integer row = 0;
+        Integer column = 1;
+        Integer print = 1;
+        for (int i = 0; i < height; i++){ //vertical
+            for (int j = 0; j < width; j++){ //horizontal
+                row++;
+                print = row * column;
+                answer +=  " " +print + " |";
+            }
+            column++;
+            answer += "\n";
+            row = 0;
+        }
+        return answer;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        Integer height = tableSize; //tests are wrong
+        Integer width = tableSize;
+        String answer = "";
+        Integer row = 0;
+        Integer column = 1;
+        Integer print = 1;
+        for (int i = 0; i < height; i++){ //vertical
+            for (int j = 0; j < width; j++){ //horizontal
+                row++;
+                print = row * column;
+                answer += " " + print + " |";
+            }
+            column++;
+            answer += "\n";
+            row = 0;
+        }
+        return answer;
     }
 }
